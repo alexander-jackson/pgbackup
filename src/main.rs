d40fa3be2ca5806ef1a2b158f079169eb66a01b9
Alexander Jackson <alexanderjackson@protonmail.com>
2024-09-07 17:21:51 +0100

    feat: get `pg_dump` running for each database

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
+use std::process::Stdio;
+
 use color_eyre::eyre::Result;
+use tokio::process::Command;
 use tokio_postgres::{Client, NoTls};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
+struct DatabaseConfig {
+    user: String,
+    password: Option<String>,
+    database: String,
+    host: String,
+    port: u16,
+}
+
+impl From<&DatabaseConfig> for tokio_postgres::Config {
+    fn from(value: &DatabaseConfig) -> Self {
+        let mut config = tokio_postgres::Config::new();
+
+        config
+            .user(value.user.clone())
+            .dbname(value.database.clone())
+            .host(value.host.clone())
+            .port(value.port);
+
+        if let Some(password) = value.password.as_deref() {
+            config.password(password.to_owned());
+        }
+
+        config
+    }
+}
+
+#[tracing::instrument(skip(client))]
 async fn discover_databases(client: &Client) -> Result<Vec<String>> {
     let query = r#"
         SELECT datname
@@ -17,7 +47,37 @@ async fn discover_databases(client: &Client) -> Result<Vec<String>> {
     let rows = client.query(query, &[]).await?;
     let databases = rows.into_iter().map(|row| row.get(0)).collect();
 
+    tracing::info!(?databases, "discovered some targets for backup");
+
     Ok(databases)
+}
+
+#[tracing::instrument(skip(config))]
+async fn backup_database(config: &DatabaseConfig, database: &str) -> Result<()> {
+    let mut command = Command::new("pg_dump");
+
+    command
+        .args(&[
+            "-h",
+            &config.host,
+            "-p",
+            &config.port.to_string(),
+            "-d",
+            database,
+            "-U",
+            &config.user,
+        ])
+        .stdout(Stdio::piped());
+
+    if let Some(password) = config.password.as_deref() {
+        command.env("PGPASSWORD", password);
+    }
+
+    let output = command.spawn()?.wait_with_output().await?;
+
+    tracing::info!(bytes = %output.stdout.len(), "got some output from the dump");
+
+    Ok(())
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -31,13 +91,15 @@ async fn main() -> Result<()> {
         )
         .init();
 
-    let (client, connection) = tokio_postgres::Config::new()
-        .user("alex")
-        .dbname("postgres")
-        .host("localhost")
-        .port(5432)
-        .connect(NoTls)
-        .await?;
+    let config = DatabaseConfig {
+        user: "alex".to_owned(),
+        database: "postgres".to_owned(),
+        host: "localhost".to_owned(),
+        port: 5432,
+        password: None,
+    };
+
+    let (client, connection) = tokio_postgres::Config::from(&config).connect(NoTls).await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -47,7 +109,9 @@ async fn main() -> Result<()> {
 
     let databases = discover_databases(&client).await?;
 
-    tracing::info!(?databases, "discovered the databases to backup");
+    for database in databases {
+        backup_database(&config, &database).await?;
+    }
 
     Ok(())
 }
